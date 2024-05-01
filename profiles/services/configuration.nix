887bf4c24e9872bfcbc7593f4c05b51cf0374088
Ryan Holt <ryan@ryanholt.net>
2024-05-01 14:55:17 -0400

    make real dumb

--- profiles/services/configuration.nix
+++ profiles/services/configuration.nix
@@ -1,9 +1,10 @@
 { userSettings, ... }:
 
 {
-  imports = [ ./base.nix
-              ( import ../../system/security/sshd.nix {
-                authorizedKeys = [ "ecdsa-sha2-nistp256 AAAAE2VjZHNhLXNoYTItbmlzdHAyNTYAAAAIbmlzdHAyNTYAAABBBOslNYCKlAhgO9vxUVt4Vq0diz35JD0f6Vtdh2zfZwyb+SI/TPC+U06TPsxS++KN+HHkQvNBcqpQ6a8qNsYsVJA="];
-                inherit userSettings; })
-            ];
+  # imports = [ ./base.nix
+  #             ( import ../../system/security/sshd.nix {
+  #               authorizedKeys = [ "ecdsa-sha2-nistp256 AAAAE2VjZHNhLXNoYTItbmlzdHAyNTYAAAAIbmlzdHAyNTYAAABBBOslNYCKlAhgO9vxUVt4Vq0diz35JD0f6Vtdh2zfZwyb+SI/TPC+U06TPsxS++KN+HHkQvNBcqpQ6a8qNsYsVJA="];
+  #               inherit userSettings; })
+  #           ];
+  imports = [];
 }
