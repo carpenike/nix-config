ffc29ddf190a5452c497e79afa9456b7e0731215
Ryan Holt <ryan@ryanholt.net>
2024-05-04 18:31:54 -0400

    add user settings

---
+++ home/ryan/default.nix
@@ -0,0 +1,36 @@
+{
+  pkgs,
+  lib,
+  inputs,
+  hostname,
+  flake-packages,
+  ...
+}:
+{
+  imports = [
+    # ../_modules
+
+    # ./secrets
+    ./hosts/${hostname}.nix
+  ];
+
+  modules = {
+
+    security = {
+      ssh = {
+        enable = true;
+      };
+    };
+
+    shell = {
+      fish.enable = true;
+
+      git = {
+        enable = true;
+        username = "Ryan Holt";
+        email = "ryan@ryanholt.net";
+      };
+    };
+
+  };
+}
