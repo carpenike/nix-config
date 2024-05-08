40f5f8f3a13b40cdf61a436cd5627a9429093308
Ryan Holt <ryan@ryanholt.net>
2024-05-08 17:06:33 -0400

    add lib

--- hosts/rydev/default.nix
+++ hosts/rydev/default.nix
@@ -57,7 +57,7 @@ in
 
         adguardhome = {
           enable = true;
-          settings = import ./config/adguard.nix {inherit config;};
+          settings = import ./config/adguard.nix {inherit config lib;};
         };
 
         chrony = {
