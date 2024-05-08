b1012d10da7fbdc1c39800b2cb713bce9848ac06
Ryan Holt <ryan@ryanholt.net>
2024-05-08 11:46:41 -0400

    attrs type

--- hosts/_modules/nixos/services/adguardhome/default.nix
+++ hosts/_modules/nixos/services/adguardhome/default.nix
@@ -12,8 +12,8 @@ in
     enable = lib.mkEnableOption "adguardhome";
     package = lib.mkPackageOption pkgs "adguardhome" { };
     settings = lib.mkOption {
-      default = config.services.adguardhome.settings;
-      type = lib.type.str;
+      default = null;
+      type = lib.types.attrs;
     };
   };
 
