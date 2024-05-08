6a0dc83c2ce35f4de70721af2862e948b23335ff
Ryan Holt <ryan@ryanholt.net>
2024-05-08 14:07:58 -0400

    see if we broke anything

--- hosts/rydev/default.nix
+++ hosts/rydev/default.nix
@@ -65,9 +65,61 @@ in
               bind_host = "127.0.0.1";
               port = 5390;
               bootstrap_dns = [
+                # quad9
+                "9.9.9.10"
+                "149.112.112.10"
+                "2620:fe::10"
+                "2620:fe::fe:10"
+
+                # cloudflare
                 "1.1.1.1"
+                "2606:4700:4700::1111"
               ];
+              upstream_dns = [
+                "[/holthome.net/]127.0.0.1:5390"
+                "[/ryho.lt/]127.0.0.1:5390"
+                "[/in-addr.arpa/]127.0.0.1:5390"
+                "[/ip6.arpa/]127.0.0.1:5390"
+                "https://1.1.1.1/dns-query"
+              ];
+              upstream_mode = "load_balance";
+              fallback_dns = [
+                "https://dns.cloudflare.com/dns-query"
+              ];
+              local_ptr_upstreams = [ "127.0.0.1:5390" ];
+              use_private_ptr_resolvers = true;
+
+              # security
+              enable_dnsseec = true;
+
+              # local cache settings
+              cache_size = 100000000;
+              cache_ttl_min = 60;
+              cache_optimistic = true;
+
+              theme = "auto";
             };
+            filters = 
+              let
+                urls = [
+                  { name = "AdGuard DNS filter"; url = "https://adguardteam.github.io/AdGuardSDNSFilter/Filters/filter.txt"; }
+                  { name = "AdAway Default Blocklist"; url = "https://adaway.org/hosts.txt"; }
+                  { name = "Big OSID"; url = "https://big.oisd.nl"; }
+                  { name = "1Hosts Lite"; url = "https://o0.pages.dev/Lite/adblock.txt"; }
+                  { name = "hagezi multi pro"; url = "https://cdn.jsdelivr.net/gh/hagezi/dns-blocklists@latest/adblock/pro.txt"; }
+                  { name = "osint"; url = "https://osint.digitalside.it/Threat-Intel/lists/latestdomains.txt"; }
+                  { name = "phishing army"; url = "https://phishing.army/download/phishing_army_blocklist_extended.txt"; }
+                  { name = "notrack malware"; url = "https://gitlab.com/quidsup/notrack-blocklists/raw/master/notrack-malware.txt"; }
+                  { name = "EasyPrivacy"; url = "https://v.firebog.net/hosts/Easyprivacy.txt"; }
+                ];
+                buildList = id: url: {
+                   enabled = true;
+                   inherit id;
+                   inherit (url) name;
+                   inherit (url) url;
+                 };
+              in
+                lib.imap1 buildList urls;
           };
         };
 
