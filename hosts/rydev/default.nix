41644822543a0881918b0b7e1e4005ccfefe1c53
Ryan Holt <ryan@ryanholt.net>
2024-05-08 16:19:09 -0400

    format it this way?

--- hosts/rydev/default.nix
+++ hosts/rydev/default.nix
@@ -58,7 +58,7 @@ in
         adguardhome = {
           enable = true;
           package = pkgs.adguardhome;
-          settings = import ./config/adguard.nix {inherit settings;};
+          (import ./config/adguard.nix);
         };
 
         chrony = {
