402a1f1d9c6966402b0d94109e3a53e954eae09a
Ryan Holt <ryan@ryanholt.net>
2024-05-08 16:04:35 -0400

    disable tumblr

--- hosts/rydev/default.nix
+++ hosts/rydev/default.nix
@@ -243,7 +243,7 @@ in
                       "tidal"
                       "tiktok"
                       "tinder"
-                      "tumblr"
+                      # "tumblr"
                       "twitch"
                       "twitter"
                       "ubisoft"
