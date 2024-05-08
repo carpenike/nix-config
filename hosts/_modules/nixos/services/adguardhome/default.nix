ba6a146593b97a363e3fd8beef2aff060722e581
Ryan Holt <ryan@ryanholt.net>
2024-05-08 18:47:12 -0400

    try to insert password this way

--- hosts/_modules/nixos/services/adguardhome/default.nix
+++ hosts/_modules/nixos/services/adguardhome/default.nix
@@ -23,5 +23,15 @@ in
       mutableSettings = false;
       inherit (cfg) settings;
     };
+    # insert password before service starts
+    # password in sops is unencrypted, so we bcrypt it
+    # and insert it as per config requirements
+    systemd.services.adguardhome = {
+      preStart = lib.mkAfter ''
+        HASH=$(cat ${config.sops.secrets."networking/adguardhome/password".path} | ${pkgs.apacheHttpd}/bin/htpasswd -niB "" | cut -c 2-)
+        ${pkgs.gnused}/bin/sed -i "s,ADGUARDPASS,$HASH," "$STATE_DIRECTORY/AdGuardHome.yaml"
+      '';
+    };
   };
+
 }
