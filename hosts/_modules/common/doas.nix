63a09bdc76dc8ebf563c8494ec038335a81b9ea1
Ryan Holt <ryan@ryanholt.net>
2024-05-08 13:53:09 -0400

    try defining no pass this way

--- hosts/_modules/common/doas.nix
+++ hosts/_modules/common/doas.nix
@@ -6,12 +6,13 @@
   # Doas instead of sudo
   security.doas.enable = true;
   security.sudo.enable = false;
-  security.doas.wheelNeedsPassword = false;
-  # security.doas.extraRules = [{
-  #   users = [ "ryan" ];
-  #   keepEnv = true;
-  #   persist = true;
-  # }];
+  # security.doas.wheelNeedsPassword = false;
+  security.doas.extraRules = [{
+    users = [ "ryan" ];
+    keepEnv = true;
+    persist = true;
+    nopass = true;
+  }];
 
   environment.systemPackages = [
     (pkgs.writeScriptBin "sudo" ''exec doas "$@"'')
