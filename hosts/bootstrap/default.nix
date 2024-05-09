545b889dc3cd26202b427689d172c8abdfe937e6
Ryan Holt <ryan@ryanholt.net>
2024-05-09 10:25:38 -0400

    nest inside config

--- hosts/bootstrap/default.nix
+++ hosts/bootstrap/default.nix
@@ -3,32 +3,34 @@
   imports =  [ ./hardware-configuration.nix ];
   networking.hostName = "nixos-bootstrap";
   networking.hostId = "a39fb76a";
-  boot.loader.systemd-boot.enable = true;
-  boot.loader.efi.canTouchEfiVariables = true;
-  environment.systemPackages = with pkgs; [
-    git
-    vim
-  ];
-  users.users.ryan = {
-    uid = 1000;
-    name = "ryan";
-    home = "/home/ryan";
-    group = "ryan";
-    shell = pkgs.fish;
-    openssh.authorizedKeys.keys = lib.strings.splitString "\n" (builtins.readFile ../../home/ryan/config/ssh/ssh.pub);
-    isNormalUser = true;
-    extraGroups =
-      [
-        "wheel"
-        "users"
-      ];
-  };
-  users.groups.ryan = {
-    gid = 1000;
-  };
-  modules = {
-    services = {
-      openssh.enable = true;
+  config = {
+    boot.loader.systemd-boot.enable = true;
+    boot.loader.efi.canTouchEfiVariables = true;
+    environment.systemPackages = with pkgs; [
+      git
+      vim
+    ];
+    users.users.ryan = {
+      uid = 1000;
+      name = "ryan";
+      home = "/home/ryan";
+      group = "ryan";
+      shell = pkgs.fish;
+      openssh.authorizedKeys.keys = lib.strings.splitString "\n" (builtins.readFile ../../home/ryan/config/ssh/ssh.pub);
+      isNormalUser = true;
+      extraGroups =
+        [
+          "wheel"
+          "users"
+        ];
+    };
+    users.groups.ryan = {
+      gid = 1000;
+    };
+    modules = {
+      services = {
+        openssh.enable = true;
+      };
     };
   };
   system.stateVersion = "23.11";
