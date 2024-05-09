826ccf7d0e133ea920c6c92e8e14e5cce718a9cc
Ryan Holt <ryan@ryanholt.net>
2024-05-09 10:01:07 -0400

    add hostId for zfs support

--- hosts/bootstrap/configuration.nix
+++ hosts/bootstrap/configuration.nix
@@ -2,6 +2,7 @@
 {
   imports =  [ ./hardware-configuration.nix ];
   networking.hostName = "nixos-bootstrap";
+  networking.hostId = "a39fb76a";
   boot.loader.systemd-boot.enable = true;
   boot.loader.efi.canTouchEfiVariables = true;
   environment.systemPackages = with pkgs; [
