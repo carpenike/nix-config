e2ccd86317f93288d3bbe91219cc2557c9b29573
Ryan Holt <ryan@ryanholt.net>
2024-05-10 16:14:00 -0400

    unneeded

--- hosts/rydev/default.nix
+++ hosts/rydev/default.nix
@@ -51,10 +51,6 @@ in
 
     modules = {
       services = {
-        dnsdist = {
-          enable = true;
-          config = builtins.readFile ./config/dnsdist.conf;
-        };
         chrony = {
           enable = true;
           servers = [
