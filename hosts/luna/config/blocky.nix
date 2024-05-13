0e30805870e105186d08626e499325340372f7ab
Ryan Holt <ryan@ryanholt.net>
2024-05-13 17:52:01 -0400

    maybe this is right

--- hosts/luna/config/blocky.nix
+++ hosts/luna/config/blocky.nix
@@ -45,13 +45,12 @@ in
   };
 
   clientLookup = {
-    clients = [
-      {
-        name = "caydanDevices";
+    clients = {
+      caydanDevices = {
         ip = [ "10.30.50.255" ];
-        # mapping = [ "kids" ];
-      }
-    ];
+        mapping = [ "kids" ];
+      };
+    };
   };
 
   blocking = {
