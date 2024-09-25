ab1e40b0c3303601e8717712124926928d01593f
Ryan Holt <ryan@ryanholt.net>
2024-09-25 14:32:20 -0400

    adjust flake to use flakeparts

--- flake.nix
+++ flake.nix
@@ -9,6 +9,15 @@
 
     hardware.url = "github:nixos/nixos-hardware";
 
+    # Flake-parts - Simplify Nix Flakes with the module system
+    #
+    flake-parts = {
+      url = "github:hercules-ci/flake-parts";
+      inputs.nixpkgs-lib.follows = "nixpkgs";
+    };
+
+    # home-manager - home user+dotfile manager
+    # https://github.com/nix-community/home-manager
     home-manager = {
       url = "github:nix-community/home-manager/release-24.05";
       inputs.nixpkgs.follows = "nixpkgs";
@@ -22,15 +31,24 @@
       inputs.nixpkgs.follows = "nixpkgs";
     };
 
-    # Secrets management.
+    # sops-nix - secrets with mozilla sops
+    # https://github.com/Mic92/sops-nix
     sops-nix = {
       url = "github:mic92/sops-nix";
       inputs.nixpkgs.follows = "nixpkgs";
     };
 
-    # vim4LMFQR!
+    # NixVim - Configure Neovim with Nix
+    # https://github.com/nix-community/nixvim
     nixvim = {
-      url = "github:nix-community/nixvim";
+      url = "github:nix-community/nixvim/nixos-24.05";
+      inputs.nixpkgs.follows = "nixpkgs";
+    };
+
+    # VSCode community extensions
+    # https://github.com/nix-community/nix-vscode-extensions
+    nix-vscode-extensions = {
+      url = "github:nix-community/nix-vscode-extensions";
       inputs.nixpkgs.follows = "nixpkgs";
     };
 
@@ -52,15 +70,11 @@
       inputs.nixpkgs.follows = "nixpkgs";
     };
 
-    # deploy-rs
-    deploy-rs = {
-      url = "github:serokell/deploy-rs";
-      inputs = {
-        nixpkgs.follows = "nixpkgs";
-      };
+    # nix-inspect - Interactive tui for inspecting nix configs
+    # https://github.com/bluskript/nix-inspect
+    nix-inspect = {
+      url = "github:bluskript/nix-inspect";
     };
-
-    nix-inspect.url = "github:bluskript/nix-inspect";
 
     impermanence.url = "github:nix-community/impermanence";
 
@@ -68,82 +82,62 @@
   };
 
   outputs = {
-    self,
-    nixpkgs,
-    nixpkgs-unstable,
-    home-manager,
-    nix-darwin,
-    nix-inspect,
-    sops-nix,
-    disko,
-    impermanence,
+    flake-parts,
     ...
   } @inputs:
   let
-    supportedSystems = ["x86_64-linux" "aarch64-darwin" "aarch64-linux"];
-    forAllSystems = nixpkgs.lib.genAttrs supportedSystems;
     overlays = import ./overlays {inherit inputs;};
-    mkSystemLib = import ./lib/mkSystem.nix {inherit inputs;};
-    flake-packages = self.packages;
-
-    legacyPackages = forAllSystems (
-      system:
-        import nixpkgs {
-          inherit system;
-          overlays = builtins.attrValues overlays;
-          config.allowUnfree = true;
-        }
-    );
+    mkSystemLib = import ./lib/mkSystem.nix {inherit inputs overlays;};
   in
   {
-    inherit overlays;
+    flake-parts.lib.mkFlake {inherit inputs;} {
+      systems = [
+        "aarch64-darwin"
+        "x86_64-linux"
+        "aarch64-linux"
+      ];
 
-    packages = forAllSystems (
-      system: let
-        pkgs = legacyPackages.${system};
-      in
-        import ./pkgs {
-          inherit pkgs;
-          inherit inputs;
-        }
-    );
+      imports = [];
 
-    # Shell configured with packages that are typically only needed when working on or with nix-config.
-    devShells = forAllSystems
-      (system:
-        let pkgs = nixpkgs.legacyPackages.${system};
-        in import ./shell.nix { inherit pkgs; }
-      );
+      flake = {
+        # Shell configured with packages that are typically only needed when working on or with nix-config.
+        devShells = forAllSystems
+          (system:
+            let pkgs = nixpkgs.legacyPackages.${system};
+            in import ./shell.nix { inherit pkgs; }
+          );
 
-    #################### NixOS Configurations ####################
-    #
-    # Building configurations available through `just rebuild` or `nixos-rebuild --flake .#hostname`
+        #################### NixOS Configurations ####################
+        #
+        # Building configurations available through `just rebuild` or `nixos-rebuild --flake .#hostname`
 
-    nixosConfigurations = {
-      # Bootstrap deployment
-      nixos-bootstrap = mkSystemLib.mkNixosSystem "x86_64-linux" "nixos-bootstrap";# overlays flake-packages;
-      # Parallels devlab
-      rydev =  mkSystemLib.mkNixosSystem "aarch64-linux" "rydev";# overlays flake-packages;
-      # Luna
-      luna =  mkSystemLib.mkNixosSystem "x86_64-linux" "luna";# overlays flake-packages;
+        nixosConfigurations = {
+          # Bootstrap deployment
+          nixos-bootstrap = mkSystemLib.mkNixosSystem "x86_64-linux" "nixos-bootstrap";# overlays flake-packages;
+          # Parallels devlab
+          rydev =  mkSystemLib.mkNixosSystem "aarch64-linux" "rydev";# overlays flake-packages;
+          # Luna
+          luna =  mkSystemLib.mkNixosSystem "x86_64-linux" "luna";# overlays flake-packages;
 
+        };
+
+        darwinConfigurations = {
+          rymac = mkSystemLib.mkDarwinSystem "aarch64-darwin" "rymac";# overlays flake-packages;
+        };
+
+        # Convenience output that aggregates the outputs for home, nixos.
+        # Also used in ci to build targets generally.
+        ciSystems =
+          let
+            nixos = nixpkgs.lib.genAttrs
+              (builtins.attrNames inputs.self.nixosConfigurations)
+              (attr: inputs.self.nixosConfigurations.${attr}.config.system.build.toplevel);
+            darwin = nixpkgs.lib.genAttrs
+              (builtins.attrNames inputs.self.darwinConfigurations)
+              (attr: inputs.self.darwinConfigurations.${attr}.system);
+          in
+            nixos // darwin;
+      };
     };
-
-    darwinConfigurations = {
-      rymac = mkSystemLib.mkDarwinSystem "aarch64-darwin" "rymac";# overlays flake-packages;
-    };
-
-    # Convenience output that aggregates the outputs for home, nixos.
-    # Also used in ci to build targets generally.
-    ciSystems =
-      let
-        nixos = nixpkgs.lib.genAttrs
-          (builtins.attrNames inputs.self.nixosConfigurations)
-          (attr: inputs.self.nixosConfigurations.${attr}.config.system.build.toplevel);
-        darwin = nixpkgs.lib.genAttrs
-          (builtins.attrNames inputs.self.darwinConfigurations)
-          (attr: inputs.self.darwinConfigurations.${attr}.system);
-      in
-        nixos // darwin;
   } // import ./deploy.nix inputs;
 }
