42b7bd643292b7c9df84814ef522d054ac77aed5
Ryan Holt <ryan@ryanholt.net>
2024-09-25 13:49:22 -0400

    disable mise

--- home/ryan/hosts/rymac.nix
+++ home/ryan/hosts/rymac.nix
@@ -15,17 +15,17 @@
     };
     kubernetes.enable = true;
     security.gnugpg.enable = true;
-    shell = {
-      mise = {
-        enable = true;
-        package = pkgs.unstable.mise;
-        globalConfig = {
-          tools = {
-            python = "latest";
-            node = "latest";
-          };
-        };
-      };
-    };
+    # shell = {
+      # mise = {
+      #   enable = true;
+      #   package = pkgs.unstable.mise;
+      #   globalConfig = {
+      #     tools = {
+      #       python = "latest";
+      #       node = "latest";
+      #     };
+      #   };
+      # };
+    # };
   };
 }
