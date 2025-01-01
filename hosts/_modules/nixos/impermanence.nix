153a3fdaabee0cf678eb70ac2b628831e097ed27
Ryan Holt <ryan@ryanholt.net>
2025-01-01 13:36:44 -0500

    update impermanence config

--- hosts/_modules/nixos/impermanence.nix
+++ hosts/_modules/nixos/impermanence.nix
@@ -21,43 +21,37 @@ with lib;
       type = lib.types.str;
       default = "/persist";
     };
-
   };
 
   config = lib.mkIf cfg.enable {
-    # move ssh keys
+    # Rollback root to blank snapshot after boot
+    boot.initrd.postDeviceCommands = lib.mkAfter ''
+      zfs rollback -r ${cfg.rootPoolName}@${cfg.rootBlankSnapshotName}
+    '';
 
-    # bind a initrd command to rollback to blank root after boot
-   boot.initrd.postDeviceCommands = lib.mkAfter ''
-     zfs rollback -r ${cfg.rootPoolName}@${cfg.rootBlankSnapshotName}
-   '';
-
-    systemd.tmpfiles.rules = mkIf config.services.openssh.enable [
-      # "d /etc/ 0755 root root -" #The - disables automatic cleanup, so the file wont be removed after a period
-      # "d /etc/ssh/ 0755 root root -" #The - disables automatic cleanup, so the file wont be removed after a period
-    ];
-
+    # Define persistence paths
     environment.persistence."${cfg.persistPath}" = {
       hideMounts = true;
-      directories =
-        [
-          "/var/log" # persist logs between reboots for debugging
-          "/var/lib/cache" # cache files (restic, nginx, contaienrs)
-          "/var/lib/nixos" # nixos state
-          "/var/lib/omada" # omada controller data
-          "/var/lib/unifi" # unifi controller data"
-
-        ];
+      directories = [
+        "/var/log"          # Persist logs between reboots for debugging
+        "/var/lib/cache"    # Cache files (e.g., restic, nginx, containers)
+        "/var/lib/nixos"    # NixOS state
+        "/var/lib/omada"    # Omada controller data
+        "/var/lib/unifi"    # Unifi controller data
+      ];
       files = [
-        "/etc/machine-id"
-        # "/etc/adjtime" # hardware clock adjustment
-        # ssh keys
-        "/etc/ssh/ssh_host_ed25519_key"
-        "/etc/ssh/ssh_host_ed25519_key.pub"
-        "/etc/ssh/ssh_host_rsa_key"
-        "/etc/ssh/ssh_host_rsa_key.pub"
+        "/etc/machine-id"                # Machine ID
+        "/etc/ssh/ssh_host_ed25519_key"  # SSH private key
+        "/etc/ssh/ssh_host_ed25519_key.pub"  # SSH public key
+        "/etc/ssh/ssh_host_rsa_key"      # RSA private key
+        "/etc/ssh/ssh_host_rsa_key.pub"  # RSA public key
       ];
     };
 
+    # Ensure persistence services wait for /persist to mount
+    systemd.services."impermanence-bind-mounts" = {
+      after = [ "persist.mount" ];
+      requires = [ "persist.mount" ];
+    };
   };
 }
