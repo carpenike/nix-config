82c3c5d69ee15fc5ccc4905bceadabd446c4ee85
Ryan Holt <ryan@ryanholt.net>
2025-10-07 20:15:11 -0400

    fix(flake): update home-manager to use release-24.11 and adjust locked dependencies

--- flake.nix
+++ flake.nix
@@ -20,7 +20,8 @@
     # home-manager - home user+dotfile manager
     # https://github.com/nix-community/home-manager
     home-manager = {
-      url = "github:nix-community/home-manager/release-25.05";
+      # url = "github:nix-community/home-manager/release-25.05";
+      url = "github:nix-community/home-manager/release-24.11";
       inputs.nixpkgs.follows = "nixpkgs";
     };
 
