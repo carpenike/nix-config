fa5d429de5d4e90fd1b306cd73ca64ccbf2d7d79
Ryan Holt <ryan@ryanholt.net>
2025-12-10 14:42:11 -0500

    feat: Add Cachix support for persistent binary caching in workflows and Nix settings

--- flake.nix
+++ flake.nix
@@ -6,10 +6,12 @@
     extra-substituters = [
       "https://nix-community.cachix.org"
       "https://cache.garnix.io"
+      "https://carpenike.cachix.org"
     ];
     extra-trusted-public-keys = [
       "nix-community.cachix.org-1:mB9FSh9qf2dCimDSUo8Zy7bkq5CX+/rkCWyvRCYg3Fs="
       "cache.garnix.io:CTFPyKSLcx5RMJKfLo5EEPUObbA78b0YQ2DTCJXqr9g="
+      "carpenike.cachix.org-1:96Z6GrfQJkkTr1f6g9z1JCGGG54CjqIRvnrupPlzEPQ="
     ];
   };
 
--- modules/common/nix.nix
+++ modules/common/nix.nix
@@ -6,12 +6,14 @@
   nix = {
     settings = {
       trusted-substituters = [
+        "https://carpenike.cachix.org"
         "https://nix-community.cachix.org"
         "https://cache.garnix.io"
         "https://numtide.cachix.org"
       ];
 
       trusted-public-keys = [
+        "carpenike.cachix.org-1:96Z6GrfQJkkTr1f6g9z1JCGGG54CjqIRvnrupPlzEPQ="
         "nix-community.cachix.org-1:mB9FSh9qf2dCimDSUo8Zy7bkq5CX+/rkCWyvRCYg3Fs="
         "cache.garnix.io:CTFPyKSLcx5RMJKfLo5EEPUObbA78b0YQ2DTCJXqr9g="
         "numtide.cachix.org-1:2ps1kLBUWjxIneOy1Ik6cQjb41X0iXVXeHigGmycPPE="
