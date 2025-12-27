c8ae07bc3ac51150a426a23b72e1cd8c9793548f
carpenike <1302442+carpenike@users.noreply.github.com>
2025-12-27 05:49:24 +0000

    chore(deps): Update packages managed by nvfetcher
    Auto-fixed hashes for: beads

--- pkgs/_sources/generated.nix
+++ pkgs/_sources/generated.nix
@@ -3,13 +3,13 @@
 {
   beads = {
     pname = "beads";
-    version = "v0.30.6";
+    version = "v0.37.0";
     src = fetchFromGitHub {
       owner = "steveyegge";
       repo = "beads";
-      rev = "v0.30.6";
+      rev = "v0.37.0";
       fetchSubmodules = false;
-      sha256 = "sha256-ImxTgBy/aMPsL9O+/VDq86me66LWhM1FZaAHW3WaJwM=";
+      sha256 = "sha256-7emhETgutmCo2q7+xWsHCdl8syfAyzLrpTJGjHWmoGo=";
     };
   };
   cooklang-cli = {
@@ -34,73 +34,6 @@
       sha256 = "sha256-Jg8sn7ytT5AWGsOpoE7V9jlqPgec3sQygYQv4zeOv3w=";
     };
     date = "2025-11-20";
-  };
-  kubecolor-catppuccin = {
-    pname = "kubecolor-catppuccin";
-    version = "1d4c2888f7de077e1a837a914a1824873d16762d";
-    src = fetchFromGitHub {
-      owner = "vkhitrin";
-      repo = "kubecolor-catppuccin";
-      rev = "1d4c2888f7de077e1a837a914a1824873d16762d";
-      fetchSubmodules = false;
-      sha256 = "sha256-gTneUh6yMcH6dVKrH00G61a+apasu9tiMyYjvNdOiOw=";
-    };
-    date = "2024-05-24";
-  };
-  kubectl-browse-pvc = {
-    pname = "kubectl-browse-pvc";
-    version = "v1.3.0";
-    src = fetchFromGitHub {
-      owner = "clbx";
-      repo = "kubectl-browse-pvc";
-      rev = "v1.3.0";
-      fetchSubmodules = false;
-      sha256 = "sha256-8O36JLNfrh+/9JqJjeeSEO88uYkoo6OXCraK385tGvM=";
-    };
-  };
-  kubectl-get-all = {
-    pname = "kubectl-get-all";
-    version = "v1.3.8";
-    src = fetchFromGitHub {
-      owner = "corneliusweig";
-      repo = "ketall";
-      rev = "v1.3.8";
-      fetchSubmodules = false;
-      sha256 = "sha256-Mau57mXS78fHyeU0OOz3Tms0WNu7HixfAZZL3dmcj3w=";
-    };
-  };
-  kubectl-klock = {
-    pname = "kubectl-klock";
-    version = "v0.8.2";
-    src = fetchFromGitHub {
-      owner = "applejag";
-      repo = "kubectl-klock";
-      rev = "v0.8.2";
-      fetchSubmodules = false;
-      sha256 = "sha256-Ajq3/JUnaIcz6FnC2nP9H/+oKJXQSca+mRpPSkG/xY0=";
-    };
-  };
-  kubectl-netshoot = {
-    pname = "kubectl-netshoot";
-    version = "v0.1.0";
-    src = fetchFromGitHub {
-      owner = "nilic";
-      repo = "kubectl-netshoot";
-      rev = "v0.1.0";
-      fetchSubmodules = false;
-      sha256 = "sha256-6IQmD2tJ1qdjeJqOnHGSpfNg6rxDRmdW9a9Eon/EdsM=";
-    };
-  };
-  kubectl-pgo = {
-    pname = "kubectl-pgo";
-    version = "v0.5.3";
-    src = fetchFromGitHub {
-      owner = "CrunchyData";
-      repo = "postgres-operator-client";
-      rev = "v0.5.3";
-      fetchSubmodules = false;
-      sha256 = "sha256-QaOa8L86YaR1GJ5+iNQO/T83yLzqWTngBr0UzovQ9yg=";
-    };
   };
   shcopy = {
     pname = "shcopy";
