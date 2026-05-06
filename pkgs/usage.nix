b4d3f70208f6c7cf6391d79692c72d70acf57722
carpenike <1302442+carpenike@users.noreply.github.com>
2026-05-06 08:00:28 +0000

    chore(deps): Update packages managed by nvfetcher
    Auto-fixed hashes for: usage

--- pkgs/usage.nix
+++ pkgs/usage.nix
@@ -13,7 +13,7 @@ in
 rustPlatform.buildRustPackage rec {
   inherit (packageData) pname src;
   version = lib.strings.removePrefix "v" packageData.version;
-  cargoHash = "sha256-JauKSCp5Fk1shzXE2dy/joYamv7XW2rnIsu4hjIkBUA=";
+  cargoHash = "sha256-O55lgNXF6buoL5JGOqXNyy532W6/QBIQ17aheW/EEwA=";
 
   # WORKAROUND (2025-02-11): 4 tests fail in complete_word test suite
   # Affects: usage-cli v2.16.1
